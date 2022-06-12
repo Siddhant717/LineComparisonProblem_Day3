fcd7a49222ae068ef1e8118f9316053ad26294c1
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 12:07:09 +0530

    Added UC1 Solution

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,6 +7,22 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            Console.WriteLine("Enter the value of x1 :");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1 :");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double x = (x2 - x1) * (x2 - x1);
+            double y = (y2 - y1) * (y2 - y1);
+            double z = x + y;
+            double length = Math.Sqrt(z);
+            Console.WriteLine("Length of the line is" + length);
         }
     }
 }
