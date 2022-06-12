c1877604aeb388eb1baf262a084dbd9de871f108
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 12:37:37 +0530

    Added UC2 Solution

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -18,11 +18,37 @@ namespace LineComparison
             Console.WriteLine("Enter the value of y2:");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine("Enter the value of x3 :");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y3 :");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x4:");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y4:");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+
             double x = (x2 - x1) * (x2 - x1);
             double y = (y2 - y1) * (y2 - y1);
             double z = x + y;
-            double length = Math.Sqrt(z);
-            Console.WriteLine("Length of the line is" + length);
+            double length1 = Math.Sqrt(z);
+            Console.WriteLine("Length of the line is" + length1);
+
+            double a = (x4 - x3) * (x4 - x3);
+            double b = (y4 - y3) * (y4 - y3);
+            double c = a + b;
+            double length2 = Math.Sqrt(c);
+            Console.WriteLine("Length of the line is" + length2);
+
+            if (length1 == length2)
+            {
+                Console.WriteLine("Both the lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both the lines are not equal");
+            }
         }
     }
 }
